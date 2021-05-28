488f0760cbb40beb9ba23cd7dbda777e1281e778
k.hiraki <k.hiraki@dolphinZ.local>
2021-05-28 14:56:12 +0900

    Fixed tracerouter.py to execute command, and display the results.

--- tracerouter.py
+++ tracerouter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import subprocess
 
 if len(sys.argv)!=3:
   print("Usage: host-list-file ToAddress")
@@ -18,3 +19,12 @@ with open(hostfile) as f:
     print("##############################################")
     print(cmd)
     print("")
+
+    proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    results = proc.communicate()
+    ( stdout, stderr ) = results
+    stdout = stdout.strip()
+    stderr = stderr.strip()
+
+    print(stdout)
+    print(stderr)
